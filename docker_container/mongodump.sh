fd9c48e0a37ced781ff6d521ef7b9b1189abf4f4
senzhang <358974225@qq.com>
2019-07-05 19:11:01 +0800

    update dockerfile

--- docker_container/mongodump.sh
+++ docker_container/mongodump.sh
@@ -1,33 +1,34 @@
 #! /bin/bash
 
-DB_HOST='localhost:27017' # mongo host
-DB_NAME='data' # mongo database
+DB_HOST=''
+DB_NAME=''
 
-OUT_DIR='/root/alpha/mongod_bak_now' # Temporary catalogue
-TAR_DIR='/root/alpha/mongod_bak_list' # Backup storage path
+OUT_DIR='/code/mongod_bak_now' #临时目录
+TAR_DIR='/code/mongod_bak_list' # 备份存放路径
 
-DB_USER='test' # mongo username
-DB_PASS='test' # mongo user password
+DB_USER=''
+DB_PASS=''
 
-DATE=$(date +'%Y-%m-%d') # Get current system time
+DATE=$(date +'%Y-%m-%d') # 获取当前系统时间
 
-OP_DIR="$OUT_DIR/$DATE" # Daily backups are placed in a folder called current time
-echo "------current time is $DATE------"
+OP_DIR="$OUT_DIR/$DATE" # 每天的备份放在一个文件夹中
+echo "------当前时间为$DATE------"
 
-DAYS=7 # Means to delete a backup from 7 days ago and keep only a backup for 7 days
+DAYS=7 # 代表删除7天前的备份，只保留7天的备份
 TAR_BAK="mongod_bak_$DATE.tar.gz"
+
 cd $OUT_DIR
 
-echo '------Delete original backup file------'
+echo '------删除原有备份文件------'
 rm -rf $OUT_DIR/*
 
 mkdir -p "$OP_DIR"
-echo '----Start backing up the database----'
+echo '----开始备份数据库----'
 
 
 mongodump -h $DB_HOST -u $DB_USER -p $DB_PASS --authenticationDatabase admin -d $DB_NAME -o $OP_DIR --forceTableScan
 
-tar -zcPvf $TAR_DIR/$TAR_BAK $OUT_DIR/$DATE # Compress the current backup file and store it in TAR_DIR
+tar -zcPvf $TAR_DIR/$TAR_BAK $OUT_DIR/$DATE
 
 find $TAR_DIR/ -mtime +$DAYS -delete
 exit
