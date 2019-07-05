cc4a9bcc7321e1f11fd2b0909422caf7020e40f0
senzhang <358974225@qq.com>
2019-07-05 11:25:09 +0800

    add document, update readme

--- readme.md
+++ readme.md
@@ -15,3 +15,21 @@ file:
 - config.json  the key is the id of file, value is the bash filename
 
 - utility.js some tools we can use
+
+- scripts bash shell script
+    
+    - hello-world  test script
+    
+    - mongodump  mongo data Backup
+        
+        ```
+        DB_HOST='' # mongo host
+        DB_NAME='' # mongo database
+        
+        OUT_DIR='' # Temporary catalogue
+        TAR_DIR='' # Backup storage path
+        
+        DB_USER='' # mongo username
+        DB_PASS='' # mongo user password
+        DAYS=7 # Keep the file for 7 days 
+        ```
--- scripts/mongodump.sh
+++ scripts/mongodump.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
-DB_HOST='' # mongo host
-DB_NAME='data'
+DB_HOST='localhost:27017' # mongo host
+DB_NAME='data' # mongo database
 
-OUT_DIR='/root/alpha/mongod_bak_now' #临时目录
-TAR_DIR='/root/alpha/mongod_bak_list' # 备份存放路径
+OUT_DIR='/root/alpha/mongod_bak_now' # Temporary catalogue
+TAR_DIR='/root/alpha/mongod_bak_list' # Backup storage path
 
-DB_USER='' # mongo username
-DB_PASS='' # mongo user password
+DB_USER='test' # mongo username
+DB_PASS='test' # mongo user password
 
-DATE=$(date +'%Y-%m-%d') # 获取当前系统时间
+DATE=$(date +'%Y-%m-%d') # Get current system time
 
-OP_DIR="$OUT_DIR/$DATE" # 每天的备份放在一个文件夹中
-echo "------当前时间为$DATE------"
+OP_DIR="$OUT_DIR/$DATE" # Daily backups are placed in a folder called current time
+echo "------current time is $DATE------"
 
-DAYS=7 # 代表删除7天前的备份，只保留7天的备份
+DAYS=7 # Means to delete a backup from 7 days ago and keep only a backup for 7 days
 TAR_BAK="mongod_bak_$DATE.tar.gz"
 cd $OUT_DIR
 
-echo '------删除原有备份文件------'
+echo '------Delete original backup file------'
 rm -rf $OUT_DIR/*
 
 mkdir -p "$OP_DIR"
-echo '----开始备份数据库----'
+echo '----Start backing up the database----'
 
 
 mongodump -h $DB_HOST -u $DB_USER -p $DB_PASS --authenticationDatabase admin -d $DB_NAME -o $OP_DIR --forceTableScan
 
-tar -zcPvf $TAR_DIR/$TAR_BAK $OUT_DIR/$DATE
+tar -zcPvf $TAR_DIR/$TAR_BAK $OUT_DIR/$DATE # Compress the current backup file and store it in TAR_DIR
 
 find $TAR_DIR/ -mtime +$DAYS -delete
 exit
