3aa25b1745af7c8a973e4b76ea539bc47da33e02
senzhang <358974225@qq.com>
2019-09-12 09:29:10 +0800

    update == -> ===

--- scripts/kill_mongo_timeout.js
+++ scripts/kill_mongo_timeout.js
@@ -1,6 +1,6 @@
 const currentOp = db.currentOp({ active: true, secs_running: { $gt: 30 } });
 for (op in currentOp.inprog) {
-  if ("192.168.1.10" == currOp.inprog[op].client.split(":")[0])
+  if ("192.168.1.10" === currOp.inprog[op].client.split(":")[0])
     {
       db.killOp(currOp.inprog[op].opid);
     }
